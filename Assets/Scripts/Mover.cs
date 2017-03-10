f0950eb4571f4fa6ba40f54f8c0faf5f354c7cfc
earldenzel <earldenzel@gmail.com>
2017-03-10 13:38:51 -0500

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    //Public fields
+    public float speed;
+
+    //Private fields
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+public class PlayerController : MonoBehaviour {
+
+    //Public fields
+    public float speed;
+
+    //Private fields
+    private Rigidbody2D rb;
+
+	// Use this for initialization (Runs ONCE, when object is created)
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    // Update is called once per frame (USED FOR PHYSICS)
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector2(Mathf.Clamp(rb.position.x, -10.00f, 10.00f), Mathf.Clamp(rb.position.y, -4f, 4f));
+    }
+}

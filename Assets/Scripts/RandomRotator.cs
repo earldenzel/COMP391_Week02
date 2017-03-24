b7580dd2536df3b2150a3337c34ac5dd99bd8d7f
earldenzel <earldenzel@gmail.com>
2017-03-24 14:15:54 -0400

    added Destroy By Contact

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.angularVelocity = tumble * Random.value;
+    }
+}

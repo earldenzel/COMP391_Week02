95d3f17986a8ad6e6b024f801c169004d24c042d
earldenzel <earldenzel@gmail.com>
2017-03-31 14:25:21 -0400

    version 1
    asteroid version 1

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion_asteroid;
+    public GameObject explosion_player;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Boundary")
         {
             return;
         }
+        if (other.tag == "Player")
+        {
+            Instantiate(explosion_player, other.transform.position, other.transform.rotation); //spawn playersplosion
+        }
+
+        Instantiate(explosion_asteroid, this.transform.position, this.transform.rotation); // spawn explosion at this asteroid
+
         Destroy(other.gameObject); //destroy laser
         Destroy(this.gameObject); //destroy asteroid
     }
